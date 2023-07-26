2893da8b59eef59f1e0b512e7ec582ab9a6a87cc
overol1221 <fiqiachmad12@gmail.com>
2023-07-26 21:39:45 +0700

    Create ReactJS

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, ReactJS!</h1>
+            <p>This is a simple ReactJS component rendered in HTML.</p>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
